49f3af6c307af1d6284347474b75d82e8fd9930e
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-01-12 10:28:28 -0300

    hello world entrega final en typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [first, setDsd] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="Añadir item" defaultValue='' />
+        <Button color='#B26E63' title="+" />
+      </View>
+      {
+        Array.from(Array(5).keys()).map((_, i) => {
+         return <Text key={i+1}>Item {i+1}</Text>
+      })
+      }
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C0CAAD',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '100%',
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',    
+    borderRadius: 5,
+    borderColor: 'black',
+    height: 40, //???????
+    width: 150,
   },
 });
