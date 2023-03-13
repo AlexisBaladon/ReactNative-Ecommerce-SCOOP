798c704cbbe556c5a3455f0bf0422e6179935401
AlexisBaladon <93690531+AlexisBaladon@users.noreply.github.com>
2023-03-13 15:03:29 -0300

    Delete env.ts

--- src/firebase/env.ts
+++
@@ -1,4 +0,0 @@
-export const API_URL = 'https://heladeria-ecommerce-default-rtdb.firebaseio.com/';
-export const API_KEY = 'AIzaSyBr3oicVK6A2HCae8JeOgCQUBlfXlNJErQ';
-export const LOGIN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
-export const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
